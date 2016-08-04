5bae9a9e199f38288edd7679bd517af7baebacd0
吉村元聖 <strg.frontier@gmail.com>
2016-08-04 10:56:27 +0900

    second commit

--- app/controllers/cycle_controller.rb
+++ app/controllers/cycle_controller.rb
@@ -1,5 +1,14 @@
 class CycleController < ApplicationController
   def index
     @articles = Article.all
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.find(1)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'cycle#index'
   get 'cycle' => 'cycle#index'
+  get 'cycle/new' => 'cycle#new'
+  get 'cycle/:id' => 'cycle#show'
 end
